6ff4b0da5d5b472b574abd05d0007fd9627bf521
RamanAuseikou <avseykov@list.ru>
2021-11-01 20:24:47 +0300

    функция ограничения минимального разрешения картики

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,26 +1,35 @@
+from PIL import Image
+
 from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 
-from PIL import Image
+
 
 
 class NotebookAdminForm(ModelForm):
 
-    MIN_RESOLUTION = (400, 400)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = 'Минимальное разрешение {}x{}'.format(
-            *self.MIN_RESOLUTION
+        self.fields['image'].help_text = mark_safe(
+            '<span style="color:red; font-size:14px;">Минимальное разрешение {}x{}</span>'.format(
+                *Product.MIN_RESOLUTION
+            )
         )
 
     def clean_image(self):
         image = self.cleaned_data['image']
         img = Image.open(image)
-        min_height, min_width = self.MIN_RESOLUTION
+        min_height, min_width = Product.MIN_RESOLUTION
+        max_height, max_width = Product.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise ValidationError('Размер изображения не должен превышать 3MB')
         if img.height < min_height or img.width < min_width:
             raise ValidationError('Разрешение изображения меньше минимального')
+        if img.height > max_height or img.width > max_width:
+            raise ValidationError('Разрешение изображения больше максимального')
         return image
 
 class NotebookAdmin(admin.ModelAdmin):
