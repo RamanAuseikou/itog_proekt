497190c902a1172fbefef23924846d4295747cae
RamanAuseikou <avseykov@list.ru>
2021-11-03 22:59:56 +0300

    применеие шаблона, html, CSS, добавил mixin

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
 from .models import Notebook, Smartphone, Category
-
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories': categories})
+from .mixins import CategoryDetailMixin
 
 
+class BaseView(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        return render(request, 'base.html', {'categories': categories})
 
 
-class ProductDetailView(DetailView):
+#def test_view(request):
+#    categories = Category.objects.get_categories_for_left_sidebar()
+#    return render(request, 'base.html', {'categories': categories})
+
+
+
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
@@ -28,7 +37,7 @@ class ProductDetailView(DetailView):
     slug_url_kwarg = 'slug'
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
